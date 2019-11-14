2496de67540ecce173af9aa7c88de4dda7c7a015
MikenTNT <miguel.sanchezgon@gmail.com>
2019-11-14 21:30:35 +0100

    Error solved in little endian.

--- bits.c
+++ bits.c
@@ -15,7 +15,7 @@ char * printBits(int nOfBytes, void *bytes)
 	char *strBits;
 
 
-	if ((strBits = (char *)malloc(strSize)) == NULL) {
+	if (NULL == (strBits = (char *)malloc(strSize))) {
 		fprintf(stderr, "Error: malloc\n");
 		return NULL;
 	}
@@ -29,35 +29,32 @@ char * printBits(int nOfBytes, void *bytes)
 	/*
 	 * Reads and writes the data.
 	 */
-	#ifdef __BIG_ENDIAN_
 	for (int i = 0; i < nOfBytes; i++)
 	{
+		#ifdef __BIG_ENDIAN_
 		sprintf(oneByte, "%hhu%hhu%hhu%hhu%hhu%hhu%hhu%hhu ",
-			   arrayBytes[i].b1,
-			   arrayBytes[i].b2,
-			   arrayBytes[i].b3,
-			   arrayBytes[i].b4,
-			   arrayBytes[i].b5,
-			   arrayBytes[i].b6,
-			   arrayBytes[i].b7,
-			   arrayBytes[i].b8);
+				arrayBytes[i].b1,
+				arrayBytes[i].b2,
+				arrayBytes[i].b3,
+				arrayBytes[i].b4,
+				arrayBytes[i].b5,
+				arrayBytes[i].b6,
+				arrayBytes[i].b7,
+				arrayBytes[i].b8);
+		#else
+		sprintf(oneByte, "%hhu%hhu%hhu%hhu%hhu%hhu%hhu%hhu ",
+				arrayBytes[i].b8,
+				arrayBytes[i].b7,
+				arrayBytes[i].b6,
+				arrayBytes[i].b5,
+				arrayBytes[i].b4,
+				arrayBytes[i].b3,
+				arrayBytes[i].b2,
+				arrayBytes[i].b1);
+		#endif
+
 		strcat(strBits, oneByte);
 	}
-	#else
-	for (int i = (nOfBytes -1); i >= 0; i--)
-	{
-		sprintf(oneByte, "%hhu%hhu%hhu%hhu%hhu%hhu%hhu%hhu ",
-			   arrayBytes[i].b8,
-			   arrayBytes[i].b7,
-			   arrayBytes[i].b6,
-			   arrayBytes[i].b5,
-			   arrayBytes[i].b4,
-			   arrayBytes[i].b3,
-			   arrayBytes[i].b2,
-			   arrayBytes[i].b1);
-		strcat(strBits, oneByte);
-	}
-	#endif
 
 
 	/*
